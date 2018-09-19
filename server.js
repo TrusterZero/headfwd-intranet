a62a762b82bbb334e4c737dd7e622a39bd898d1c
Jurmen Hughes <jurmen1992@gmail.com>
2018-09-19 11:35:08 +0200

    Heroku settings directory change

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(`${__dirname}/dist`));
-
+app.use(express.static(`${__dirname}\\dist`));
+console.log(`${__dirname}\\dist`)
 app.listen(process.env.PORT || 3000)
 
 app.get('/*', (req,res) => {
